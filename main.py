7ab5cff70ab498af822b97a4599dbcb09a564bf7
jay <kumarjay2107@gmail.com>
2021-01-04 15:15:54 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, request, render_template, jsonify
+from twitter import TwitterClient
 
 app = Flask(__name__)
+
+api = TwitterClient('Data')
+
+
+def strtobool(v):
+    return v.lower() in ['yes', 'true', 't', 'l']
 
 
 @app.route('/')
@@ -8,5 +16,20 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= 5000)
+@app.route('/tweets')
+def tweets():
+    retweets_only = request.args.get('retweets_only')
+    api.set_retweet_checking(strtobool(retweets_only.lower()))
+    with_sentiment = request.args.get('with_sentiment')
+    api.set_with_sentiment(strtobool(with_sentiment.lower()))
+    query = request.args.get('query')
+    api.set_query(query)
+
+    tweets = api.get_tweets()
+
+    return jsonify({'data': tweets, 'count': len(tweets)})
+
+
+port = int(os.environ.get('PORT', 9000))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
